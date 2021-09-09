02a7264d3caa5b4ead237cf366319731ed2c90b1
isaachan <isaachanstar@gmail.com>
2021-09-09 08:43:30 +0800

    extract class.

--- user/src/main/java/com/daimler/architecture/model/UserController.java
+++ user/src/main/java/com/daimler/architecture/model/UserController.java
@@ -44,31 +44,3 @@ public class UserController {
     }
 }
 
-class UserTopicValue {
-
-    private String action;
-    private User content;
-
-    public UserTopicValue(String action, User content) {
-        this.action = action;
-        this.content = content;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    public void setAction(String action) {
-        this.action = action;
-    }
-
-    public User getContent() {
-        return content;
-    }
-
-    public void setContent(User content) {
-        this.content = content;
-    }
-
-
-}
