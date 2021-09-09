cc5a580578adedcea236dae06a9622863823d211
isaachan <isaachanstar@gmail.com>
2021-09-09 08:29:25 +0800

    delet tweet event

--- tweet/src/main/java/com/daimler/architecture/model/TweetController.java
+++ tweet/src/main/java/com/daimler/architecture/model/TweetController.java
@@ -9,19 +9,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TweetController {
 
-    private static final String CREATE_TWEET_TOPIC = "create_tweet_topic";
+    private static final String TWEETS_TOPIC = "tweets_topic";
+    
     @Autowired
     private KafkaProducer<String, String> kafkaProducer;
 
     @PostMapping("/tweets/")
     public void postTweet(@RequestBody Tweet tweet) {
         System.out.println("Post tweet " + tweet);
-        kafkaProducer.send(new ProducerRecord<>(CREATE_TWEET_TOPIC, String.valueOf(tweet.getId()), convertToJson(tweet)));
+        kafkaProducer.send(new ProducerRecord<>(TWEETS_TOPIC, String.valueOf(tweet.getId()), convertToJson(tweet)));
     }
 
     @DeleteMapping("/tweets/{id}")
     public void deleteTweet(@PathVariable long id) {
         System.out.println("Delete tweet with id " + id);
+        kafkaProducer.send(new ProducerRecord<>(TWEETS_TOPIC, String.valueOf(id), null));
     }
 
     private String convertToJson(Tweet tweet) {
