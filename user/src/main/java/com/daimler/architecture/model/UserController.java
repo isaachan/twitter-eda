647432ade2690ba72b0a25242580146ba0f9db2f
isaachan <isaachanstar@gmail.com>
2021-09-09 08:42:18 +0800

    UserController

--- user/src/main/java/com/daimler/architecture/model/UserController.java
+++ user/src/main/java/com/daimler/architecture/model/UserController.java
@@ -1,23 +1,74 @@
 package com.daimler.architecture.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private KafkaProducer<String, String> kafkaProducer;
+
+    private static final String USERS_TOPIC = "users_topic";
+
     @PostMapping("/users/")
     public void newUser(@RequestBody User u) {
-
+        kafkaProducer.send(new ProducerRecord<>(
+                USERS_TOPIC,
+                String.valueOf(u.getId()),
+                convertToJson(new UserTopicValue("create", u))));
     }
 
     @PutMapping("/users/")
     public void updateUser(@RequestBody User u) {
-
+        kafkaProducer.send(new ProducerRecord<>(
+                USERS_TOPIC,
+                String.valueOf(u.getId()),
+                convertToJson(new UserTopicValue("update", u))));
     }
 
     @GetMapping("/users/{id}")
     public User findBy(@PathVariable("id") Long id) {
         return null;
     }
+
+    private String convertToJson(Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class UserTopicValue {
+
+    private String action;
+    private User content;
+
+    public UserTopicValue(String action, User content) {
+        this.action = action;
+        this.content = content;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public User getContent() {
+        return content;
+    }
+
+    public void setContent(User content) {
+        this.content = content;
+    }
+
+
 }
