6c98a976187502213863704ce1a25935fdf84ed1
isaachan <isaachanstar@gmail.com>
2021-09-09 08:01:13 +0800

    Send event when create tweet

--- tweet/src/main/java/com/daimler/architecture/model/TweetController.java
+++ tweet/src/main/java/com/daimler/architecture/model/TweetController.java
@@ -1,17 +1,34 @@
 package com.daimler.architecture.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TweetController {
 
+    private static final String CREATE_TWEET_TOPIC = "create_tweet_topic";
+    @Autowired
+    private KafkaProducer<String, String> kafkaProducer;
+
     @PostMapping("/tweets/")
     public void postTweet(@RequestBody Tweet tweet) {
         System.out.println("Post tweet " + tweet);
+        kafkaProducer.send(new ProducerRecord<>(CREATE_TWEET_TOPIC, String.valueOf(tweet.getId()), convertToJson(tweet)));
     }
 
     @DeleteMapping("/tweets/{id}")
     public void deleteTweet(@PathVariable long id) {
         System.out.println("Delete tweet with id " + id);
+    }
+
+    private String convertToJson(Tweet tweet) {
+        try {
+            return new ObjectMapper().writeValueAsString(tweet);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
