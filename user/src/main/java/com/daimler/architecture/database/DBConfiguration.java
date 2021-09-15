4c5385e92bc0ab0352caee7cc1647a213f61ba5e
Han Kai <isaachanstar@gmail.com>
2021-09-15 22:18:38 +0800

    DBConfiguration

--- user/src/main/java/com/daimler/architecture/database/DBConfiguration.java
+++ user/src/main/java/com/daimler/architecture/database/DBConfiguration.java
@@ -1,7 +1,23 @@
 package com.daimler.architecture.database;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 public class DBConfiguration {
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public DBWriter createDBWriter() throws SQLException {
+        String url = null;
+        Connection conn = DriverManager.getConnection(url, "", "");
+        return new DBWriter(conn);
+    }
 }
--- user/src/main/java/com/daimler/architecture/database/DBWriter.java
+++ user/src/main/java/com/daimler/architecture/database/DBWriter.java
@@ -2,7 +2,16 @@ package com.daimler.architecture.database;
 
 import com.daimler.architecture.model.User;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DBWriter {
+
+    private final Connection connection;
+
+    public DBWriter(Connection conn) {
+        this.connection = conn;
+    }
 
     public void createUser(User user) {}
 
@@ -11,4 +20,8 @@ public class DBWriter {
     public void addFollower(Long target, Long follower) {}
 
     public void deleteFollower(Long target, Long follower) {}
+
+    public void close() throws SQLException {
+        connection.close();
+    }
 }
