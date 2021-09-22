dbb088a070c7eeb81ba30ca4c32f9b65caef26cd
isaachan <isaachanstar@gmail.com>
2021-09-22 08:36:52 +0800

    TweetsCountApplication

---
+++ tweetscount/src/main/java/com/daimler/architecture/TweetsCountApplication.java
@@ -0,0 +1,13 @@
+package com.daimler.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetsCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetsCountApplication.class, args);
+    }
+    
+}
