cd8d9cf27b5e9fc298d7afac984f9b6c3d81ef17
Han Kai <isaachanstar@gmail.com>
2021-09-27 06:03:20 +0800

    enhance event scheme.

--- tweet/src/main/java/com/daimler/tweet/TweetController.java
+++ tweet/src/main/java/com/daimler/tweet/TweetController.java
@@ -1,7 +1,9 @@
 package com.daimler.tweet;
 
+import com.daimler.tweet.model.Event;
 import com.daimler.tweet.model.Retweet;
 import com.daimler.tweet.model.Tweet;
+import com.daimler.tweet.model.WithSender;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -20,25 +22,34 @@ public class TweetController {
     @PostMapping("/tweets/")
     public void postTweet(@RequestBody Tweet tweet) {
         System.out.println("Post tweet " + tweet);
-        kafkaProducer.send(new ProducerRecord<>(TWEETS_TOPIC, String.valueOf(tweet.getSender()), convertToJson(tweet)));
+        kafkaProducer.send(new ProducerRecord<>(TWEETS_TOPIC, String.valueOf(tweet.getSender()), convertCreateEvent(tweet)));
     }
 
     @DeleteMapping("/tweets/{id}")
     public void deleteTweet(@PathVariable long id, @RequestBody Tweet tweet) {
         System.out.println("Delete tweet with id " + id);
-        kafkaProducer.send(new ProducerRecord<>(TWEETS_TOPIC, String.valueOf(tweet.getSender()), null));
+        kafkaProducer.send(new ProducerRecord<>(TWEETS_TOPIC, String.valueOf(tweet.getSender()), convertDeleteEvent(tweet)));
     }
 
     @PostMapping("/retweets/")
     public void retweet(@RequestBody Retweet retweet) {
         System.out.println("retweet the tweet " + retweet.getOriginalId());
-        kafkaProducer.send(new ProducerRecord<>(RETWEETS_TOPIC, String.valueOf(retweet.getSender()), convertToJson(retweet)));
+        kafkaProducer.send(new ProducerRecord<>(RETWEETS_TOPIC, String.valueOf(retweet.getSender()), convertCreateEvent(retweet)));
     }
 
     @DeleteMapping("/retweets/{id}")
     public void deleteRetweet(@PathVariable long id, @RequestBody Retweet retweet) {
         System.out.println("delete retweet " + id);
-        kafkaProducer.send(new ProducerRecord<>(RETWEETS_TOPIC, String.valueOf(retweet.getSender()), null));
+        kafkaProducer.send(new ProducerRecord<>(RETWEETS_TOPIC, String.valueOf(retweet.getSender()), convertDeleteEvent(retweet)));
+    }
+
+
+    private String convertCreateEvent(WithSender tweet) {
+        return convertToJson(new Event(Event.CREATE, tweet));
+    }
+
+    private String convertDeleteEvent(WithSender tweet) {
+        return convertToJson(new Event(Event.DELETE, tweet));
     }
 
     private String convertToJson(Object object) {
--- tweet/src/main/java/com/daimler/tweet/model/Event.java
+++ tweet/src/main/java/com/daimler/tweet/model/Event.java
@@ -2,12 +2,24 @@ package com.daimler.tweet.model;
 
 public class Event<T extends WithSender> {
 
+    public static final String CREATE = "create_action";
+    public static final String DELETE = "delete_action";
     private T content;
+    private String action;
 
     public Event() {}
 
-    public Event(T content) {
+    public Event(String action, T content) {
+        this.action = action;
         this.content = content;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
     }
 
     public T getContent() {
