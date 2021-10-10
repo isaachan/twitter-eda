a1369089ee99f47b27ebda236365d1387a0d3a86
Han Kai <isaachanstar@gmail.com>
2021-10-10 22:39:31 +0800

    replace deprecated api.

--- tweetscount/src/main/java/com/daimler/tweetcount/TweetsCountController.java
+++ tweetscount/src/main/java/com/daimler/tweetcount/TweetsCountController.java
@@ -1,6 +1,8 @@
 package com.daimler.tweetcount;
 
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StoreQueryParameters;
+import org.apache.kafka.streams.state.QueryableStoreType;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,8 @@ public class TweetsCountController {
     @GetMapping("/tweets/{userId}/counts")
     public Long postTweet(@PathVariable long userId) {
         String storeName = TweetsCountApplication.TWEET_COUNT_STATE_STORE;
-        var store = stream.store(storeName, QueryableStoreTypes.<Long, Long>keyValueStore());
+        var queryableStoreType = QueryableStoreTypes.<Long, Long>keyValueStore();
+        var store = stream.store(StoreQueryParameters.fromNameAndType(storeName, queryableStoreType));
         var count = store.get(userId);
         if (count == null) throw new UserNotFoundException(userId);
         else return count;
