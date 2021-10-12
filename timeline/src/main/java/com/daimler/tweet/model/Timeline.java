e2fc9eff93b8b523799b5346aae9aa9549a84c81
Han Kai <isaachanstar@gmail.com>
2021-10-12 17:27:10 +0800

    merge tweet into timeline.

--- timeline/src/main/java/com/daimler/tweet/model/Timeline.java
+++ timeline/src/main/java/com/daimler/tweet/model/Timeline.java
@@ -1,14 +1,43 @@
 package com.daimler.tweet.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
 public class Timeline {
 
     private String sender;
+    private Map<String, List<Tweet>> timeline = new HashMap<>();
 
     public Timeline(String sender) {
         this.sender = sender;
     }
 
     public void merge(String value) {
+        try {
+            doMerge(value);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private void doMerge(String value) throws JsonProcessingException {
+        var rawData = new ObjectMapper().readValue(value, HashMap.class);
+        var content = (HashMap) rawData.get("content");
+        var tweet = new Tweet();
+        tweet.setSender(Long.valueOf((int) rawData.get("sender")));
+        tweet.setTimeline((long) content.get("timeline"));
+        tweet.setContent((String) content.get("content"));
+
+        var tweetsOfDay = new ArrayList<Tweet>();
+        tweetsOfDay.add(tweet);
+        timeline.put("2021-10-12", tweetsOfDay);
+    }
+
+    public List<com.daimler.tweet.model.Tweet> get(String date) {
+        return timeline.get(date);
     }
 }
--- timeline/src/test/java/com/daimler/tweet/model/TimelineTests.java
+++ timeline/src/test/java/com/daimler/tweet/model/TimelineTests.java
@@ -2,11 +2,24 @@ package com.daimler.tweet.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TimelineTests {
 
     @Test
     public void add_tweet_by_time() {
         var timeline = new Timeline("1");
-        timeline.merge("");
+
+        // coming event is raised at 2021-10-12 3pm.
+        timeline.merge("{\"content\":{\"id\":1000,\"content\":\"This is a tweet.\",\"sender\":1,\"timeline\":1634021617947},\"action\":\"create_action\",\"sender\":1}");
+        List<Tweet> tweetsOfTheDay = timeline.get("2021-10-12");
+        assertEquals(1, tweetsOfTheDay.size());
     }
+
 }
