7855b3b7e02daca190171b1c602b136f0a484dc2
Han Kai <isaachanstar@gmail.com>
2021-10-16 22:25:16 +0800

    API definition of TimelineController.

--- timeline/src/main/java/com/daimler/tweet/TimelineController.java
+++ timeline/src/main/java/com/daimler/tweet/TimelineController.java
@@ -1,7 +1,18 @@
 package com.daimler.tweet;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.daimler.tweet.model.Tweet;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TimelineController {
+
+    @GetMapping("/users/{senderid}/tweets?from={from}&to={to}")
+    public List<Tweet> findTweetsBetween(@PathVariable Long senderId,
+                                         @RequestParam("from") Optional<String> from,
+                                         @RequestParam("to") Optional<String> to) {
+        return null;
+    }
 }
